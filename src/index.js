ac455c9e8bb41681bef602bc3057e60cea5cc505
NurislamAituarov <nurislamajtuarov@gmail.com>
2022-03-10 13:55:06 +0600

    last commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-// require('./index.css');
 import './index.css';
-
 const result = document.querySelector('.result');
 const inpParty = document.querySelector('#party'),
   name_short = document.getElementById('name_short'),
   name_full = document.getElementById('name_full'),
   inn_kpp = document.getElementById('inn_kpp'),
-  address = document.getElementById('address');
-const show_list = document.querySelector('.show_list');
-const li = document.querySelectorAll('li'),
-  span = document.querySelectorAll('.span');
+  address = document.getElementById('address'),
+  show_list = document.querySelector('.show_list'),
+  li = document.querySelectorAll('li'),
+  span = document.querySelectorAll('.span'),
+  p = document.querySelector('#type'),
+  actionsP = document.querySelector('#action');
 
 const url = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party';
 const token = 'a96d79dffeeb431d6669017ec478fe6a7c88ac61';
 
 const arrList = [];
 let index;
-const p = document.querySelector('p');
-p.style.marginTop = '15px';
 
 li.forEach((el, i) => {
   el.addEventListener('click', () => {
@@ -27,10 +25,6 @@ li.forEach((el, i) => {
     name_full.value = item.data.name.full_with_opf;
     inn_kpp.value = `${item.data.inn} / ${item.data.kpp}`;
     address.value = `${item.data.address.data.postal_code}, ${item.data.address.value}`;
-
-    p.innerHTML = `Организация (${item.data.type})`;
-    result.prepend(p);
-
     inpParty.value = '';
     show_list.style.display = 'none';
   });
@@ -54,11 +48,24 @@ async function request(query) {
   };
   const response = await fetch(url, options);
   const data = await response.json();
-  data.suggestions.slice(0, 5).forEach((item, i) => {
-    li[i].innerHTML = item.value;
-    span[i].innerHTML = `${item.data.address.data.postal_code}, ${item.data.address.value}`;
-  });
-  arrList.push(await data.suggestions);
+
+  if (data.suggestions.length) {
+    data.suggestions.slice(0, 5).forEach((item, i) => {
+      [...show_list.children].forEach((item) => (item.style.display = 'block'));
+
+      actionsP.innerHTML = 'Выберите вариант или продолжите ввод';
+      li[i].innerHTML = item.value;
+      span[i].innerHTML = `${item.data.address.data.postal_code}, ${item.data.address.value}`;
+      p.innerHTML = `Организация (${item.data.type})`;
+      result.prepend(p);
+    });
+    arrList.push(await data.suggestions);
+  } else {
+    [...show_list.children].forEach((item) => (item.style.display = 'none'));
+    actionsP.innerHTML = 'Неизвестная организация';
+    actionsP.style.display = 'block';
+    show_list.prepend(actionsP);
+  }
 
   return await data.suggestions;
 }
